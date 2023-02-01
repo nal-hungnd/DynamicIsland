63f17a75cb2e36cb12f40133bd7a510215d57160
Hung Ngo Dinh <hungnd@nal.vn>
2023-02-01 13:45:45 +0700

    first-commit

---
+++ ios/aaaaa/aaaaaBundle.swift
@@ -0,0 +1,17 @@
+//
+//  aaaaaBundle.swift
+//  aaaaa
+//
+//  Created by NALS_MACBOOK_112 on 30/01/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct aaaaaBundle: WidgetBundle {
+    var body: some Widget {
+        aaaaa()
+        aaaaaLiveActivity()
+    }
+}
